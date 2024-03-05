215efd284699e67df9819b6c90ef60b90ac88737
shafayetejaman <shafayetejaman@gmail.com>
2024-03-05 17:16:21 +0000

    Tue, Mar 5, 2024, 11:16 PM +06:00

--- module 9/project1/app1/urls.py
+++ module 9/project1/app1/urls.py
@@ -20,5 +20,5 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index)
+    path("app/", views.index)
 ]
--- module 9/project1/project1/urls.py
+++ module 9/project1/project1/urls.py
@@ -21,6 +21,5 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("app", views.index)
   
 ]
