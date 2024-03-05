1452099614a1f0883e53552305c201a380cd148d
shafayetejaman <shafayetejaman@gmail.com>
2024-03-05 15:49:37 +0000

    Tue, Mar 5, 2024, 9:49 PM +06:00

--- module 9/project1/project1/settings.py
+++ module 9/project1/project1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app1",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "project1.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['templates'],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
