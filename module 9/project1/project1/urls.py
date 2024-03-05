e790ff5bab8219e1ffc9bb8c1e58a61f9d4545dc
shafayetejaman <shafayetejaman@gmail.com>
2024-03-05 17:08:54 +0000

    Tue, Mar 5, 2024, 11:08 PM +06:00

--- module 9/project1/project1/urls.py
+++ module 9/project1/project1/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from . import app1
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("app1/"))
+  
 ]
