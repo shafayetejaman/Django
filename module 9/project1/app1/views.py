4de619eae0b220a391bd50897623ff042c92ac48
shafayetejaman <shafayetejaman@gmail.com>
2024-03-06 07:55:27 +0000

    Wed, Mar 6, 2024, 1:55 PM +06:00

--- module 9/project1/app1/views.py
+++ module 9/project1/app1/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    d = {a:12}
-    return render(request, "app1/index.html")
+    d = {'author' : 'Rahim', 'age' : 5}
+    return render(request, "app1/index.html", d)
