2590e3725b03962d38a1de47a223260ad019d55e
shafayetejaman <shafayetejaman@gmail.com>
2024-03-06 08:39:55 +0000

    Wed, Mar 6, 2024, 2:39 PM +06:00

--- module 9/project1/app1/views.py
+++ module 9/project1/app1/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    d = {"shope" : 50,
+    d = { {"shope" : 50,
          "oil" : 300,
-         "fruit" : 30
+         "fruit" : 30}
          }
     return render(request, "app1/index.html", d)
