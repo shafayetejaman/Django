ee3e9a83259de3c49c25729ccec4eb02160fbcdc
shafayetejaman <shafayetejaman@gmail.com>
2024-03-09 07:11:16 +0000

    Sat, Mar 9, 2024, 1:11 PM +06:00

--- module 10.5/project_10_5/app1/views.py
+++ module 10.5/project_10_5/app1/views.py
@@ -5,11 +5,11 @@ from django.shortcuts import render
 
 def home(request):
     data = [
-        {"id": 1, "name": ["Introduction", "to", "Python"], "fee": 5000},
-        {"id": 2, "name": ["Introduction", "to", "Django"], "fee": 10000},
-        {"id": 3, "name": ["Introduction", "to", "c++"], "fee": 1000},
-        {"id": 4, "name": ["Introduction", "to", "Java"], "fee": 2000},
-        {"id": 5, "name": ["Introduction", "to", "C#"], "fee": 3000},
+        {"id": 1, "name": ["Introduction", "to", "Python"], "fee": 5000 , "date":},
+        {"id": 2, "name": ["Introduction", "to", "Django"], "fee": 10000 , "date":},
+        {"id": 3, "name": ["Introduction", "to", "c++"], "fee": 1000 , "date":},
+        {"id": 4, "name": ["Introduction", "to", "Java"], "fee": 2000} , "date":,
+        {"id": 5, "name": ["Introduction", "to", "C#"], "fee": 3000 , "date":},
     ]
 
     return render(request, "app1/home.html", {"course": data})
