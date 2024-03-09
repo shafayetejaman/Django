db254ad02474596a126957d084927c373ad668a3
shafayetejaman <shafayetejaman@gmail.com>
2024-03-09 07:21:23 +0000

    Sat, Mar 9, 2024, 1:21 PM +06:00

--- module 10.5/project_10_5/app1/views.py
+++ module 10.5/project_10_5/app1/views.py
@@ -10,31 +10,31 @@ def home(request):
             "id": 1,
             "name": ["Introduction", "to", "Python"],
             "fee": 5000,
-            "date": date.today(),
+            "date": date.today() + timedelta(days=10),
         },
         {
             "id": 2,
             "name": ["Introduction", "to", "Django"],
             "fee": 10000,
-            "date": date.today(),
+            "date": date.today() + timedelta(days=50),
         },
         {
             "id": 3,
             "name": ["Introduction", "to", "c++"],
             "fee": 1000,
-            "date": date.today(),
+            "date": date.today() + timedelta(days=100),
         },
         {
             "id": 4,
             "name": ["Introduction", "to", "Java"],
             "fee": 2000,
-            "date": date.today(),
+            "date": date.today() + timedelta(days=130),
         },
         {
             "id": 5,
             "name": ["Introduction", "to", "C#"],
             "fee": 3000,
-            "date": date.today(),
+            "date": date.today() + timedelta(days=120),
         },
     ]
 
