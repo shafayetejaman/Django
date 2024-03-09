66b5b5b0572acd715fd1939656c19525caa85dd4
shafayetejaman <shafayetejaman@gmail.com>
2024-03-09 06:55:36 +0000

    Sat, Mar 9, 2024, 12:55 PM +06:00

--- module 10.5/project_10_5/app1/views.py
+++ module 10.5/project_10_5/app1/views.py
@@ -4,49 +4,32 @@ from django.shortcuts import render
 
 
 def home(request):
-    books = [
+    data = [
         {
-            "ID": 1,
-            "Title": "The Great Gatsby",
-            "Author": "F. Scott Fitzgerald",
-            "Genre": "Fiction",
-            "Year Published": 1925,
+            'id' : 1,
+            'name' : 'Python',
+            'fee' : 5000
         },
         {
-            "ID": 2,
-            "Title": "To Kill a Mockingbird",
-            "Author": "Harper Lee",
-            "Genre": "Fiction",
-            "Year Published": 1960,
+            'id' : 2,
+            'name' : 'Django',
+            'fee' : 10000 
         },
         {
-            "ID": 3,
-            "Title": "1984",
-            "Author": "George Orwell",
-            "Genre": "Dystopian",
-            "Year Published": 1949,
+            'id' : 3,
+            'name' : 'C',
+            'fee' : 1000 
         },
         {
-            "ID": 4,
-            "Title": "Pride and Prejudice",
-            "Author": "Jane Austen",
-            "Genre": "Romance",
-            "Year Published": 1813,
+            'id' : 4,
+            'name' : 'C++',
+            'fee' : 1000 
         },
         {
-            "ID": 5,
-            "Title": "Moby-Dick",
-            "Author": "Herman Melville",
-            "Genre": "Adventure",
-            "Year Published": 1851,
-        },
-        {
-            "ID": 6,
-            "Title": "War and Peace",
-            "Author": "Leo Tolstoy",
-            "Genre": "Historical",
-            "Year Published": 1869,
+            'id' : 3,
+            'name' : 'C',
+            'fee' : 1000 
         },
     ]
 
-    return render(request, "app1/home.html", {"data": books})
+    return render(request, "app1/home.html", {"course": data})
