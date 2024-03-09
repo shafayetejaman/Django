7151f93b8b6b715b1bd4893c2dbe68b3689cf6dd
shafayetejaman <shafayetejaman@gmail.com>
2024-03-09 06:49:30 +0000

    Sat, Mar 9, 2024, 12:49 PM +06:00

--- module 10.5/project_10_5/app1/views.py
+++ module 10.5/project_10_5/app1/views.py
@@ -6,42 +6,42 @@ from django.shortcuts import render
 def home(request):
     books = [
         {
-            "Book ID": 1,
+            "ID": 1,
             "Title": "The Great Gatsby",
             "Author": "F. Scott Fitzgerald",
             "Genre": "Fiction",
             "Year Published": 1925,
         },
         {
-            "Book ID": 2,
+            "ID": 2,
             "Title": "To Kill a Mockingbird",
             "Author": "Harper Lee",
             "Genre": "Fiction",
             "Year Published": 1960,
         },
         {
-            "Book ID": 3,
+            "ID": 3,
             "Title": "1984",
             "Author": "George Orwell",
             "Genre": "Dystopian",
             "Year Published": 1949,
         },
         {
-            "Book ID": 4,
+            "ID": 4,
             "Title": "Pride and Prejudice",
             "Author": "Jane Austen",
             "Genre": "Romance",
             "Year Published": 1813,
         },
         {
-            "Book ID": 5,
+            "ID": 5,
             "Title": "Moby-Dick",
             "Author": "Herman Melville",
             "Genre": "Adventure",
             "Year Published": 1851,
         },
         {
-            "Book ID": 6,
+            "ID": 6,
             "Title": "War and Peace",
             "Author": "Leo Tolstoy",
             "Genre": "Historical",
@@ -49,4 +49,4 @@ def home(request):
         },
     ]
 
-    return render(request, "app1/home.html", {'data': books})
+    return render(request, "app1/home.html", {"data": books})
