d969d9679602aea5afa1561e8f5a208c85dd6caa
shafayetejaman <shafayetejaman@gmail.com>
2024-03-09 05:47:44 +0000

    Sat, Mar 9, 2024, 11:47 AM +06:00

--- module 10.5/project_10_5/project_10_5/urls.py
+++ module 10.5/project_10_5/project_10_5/urls.py
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("app/", include("app1.urls"))
+    path("app/", include("app1.urls")),
     path("", views.home)
 ]
