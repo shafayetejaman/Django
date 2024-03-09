bbacef1a1f9d31b10c341d684cf674309d68f09e
shafayetejaman <shafayetejaman@gmail.com>
2024-03-09 07:04:40 +0000

    Sat, Mar 9, 2024, 1:04 PM +06:00

--- module 10.5/project_10_5/app1/views.py
+++ module 10.5/project_10_5/app1/views.py
@@ -5,11 +5,11 @@ from django.shortcuts import render
 
 def home(request):
     data = [
-        {"id": 1, "name": "Introduction to Python", "fee": 5000},
-        {"id": 2, "name": "Introduction to  Django", "fee": 10000},
-        {"id": 3, "name": "Introduction to C", "fee": 1000},
-        {"id": 4, "name": "Introduction to C++", "fee": 2000},
-        {"id": 5, "name": "Introduction to C#", "fee": 3000},
+        {"id": 1, "name": ["Introduction", "to", "Python"], "fee": 5000},
+        {"id": 2, "name": ["Introduction", "to", "Python"], "fee": 10000},
+        {"id": 3, "name": ["Introduction", "to", "Python"], "fee": 1000},
+        {"id": 4, "name": ["Introduction", "to", "Python"], "fee": 2000},
+        {"id": 5, "name": ["Introduction", "to", "Python"], "fee": 3000},
     ]
 
     return render(request, "app1/home.html", {"course": data})
