e657cf83fe51a3280e13f7281482481752e2a442
shafayetejaman <shafayetejaman@gmail.com>
2024-03-09 07:15:21 +0000

    Sat, Mar 9, 2024, 1:15 PM +06:00

--- module 10.5/project_10_5/app1/views.py
+++ module 10.5/project_10_5/app1/views.py
@@ -1,15 +1,41 @@
 from django.shortcuts import render
+from datetime import timedelta, date
 
 # Create your views here.
 
 
 def home(request):
     data = [
-        {"id": 1, "name": ["Introduction", "to", "Python"], "fee": 5000 , "date":},
-        {"id": 2, "name": ["Introduction", "to", "Django"], "fee": 10000 , "date":},
-        {"id": 3, "name": ["Introduction", "to", "c++"], "fee": 1000 , "date":},
-        {"id": 4, "name": ["Introduction", "to", "Java"], "fee": 2000} , "date":,
-        {"id": 5, "name": ["Introduction", "to", "C#"], "fee": 3000 , "date":},
+        {
+            "id": 1,
+            "name": ["Introduction", "to", "Python"],
+            "fee": 5000,
+            "date": date.tody(),
+        },
+        {
+            "id": 2,
+            "name": ["Introduction", "to", "Django"],
+            "fee": 10000,
+            "date": date.tody(),
+        },
+        {
+            "id": 3,
+            "name": ["Introduction", "to", "c++"],
+            "fee": 1000,
+            "date": date.tody(),
+        },
+        {
+            "id": 4,
+            "name": ["Introduction", "to", "Java"],
+            "fee": 2000,
+            "date": date.tody(),
+        },
+        {
+            "id": 5,
+            "name": ["Introduction", "to", "C#"],
+            "fee": 3000,
+            "date": date.tody(),
+        },
     ]
 
     return render(request, "app1/home.html", {"course": data})
