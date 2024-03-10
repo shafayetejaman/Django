51ac77c38122c0eda70925fc4e14557f7e6afb4e
shafayetejaman <shafayetejaman@gmail.com>
2024-03-10 09:34:52 +0000

    Sun, Mar 10, 2024, 3:34 PM +06:00

--- module 11.5/project_11_5/app1/urls.py
+++ module 11.5/project_11_5/app1/urls.py
@@ -20,6 +20,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
+    path("", views.index),
 
 ]
--- module 11.5/project_11_5/app1/views.py
+++ module 11.5/project_11_5/app1/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def home(request):
+def index(request):
     meals = [
         {
             "strMeal": "BeaverTails",
