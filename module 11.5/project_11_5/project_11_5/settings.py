e48ebc3e285bf35ded29329d5664f7289a4569bc
shafayetejaman <shafayetejaman@gmail.com>
2024-03-10 08:33:24 +0000

    Sun, Mar 10, 2024, 2:33 PM +06:00

--- module 11.5/project_11_5/project_11_5/settings.py
+++ module 11.5/project_11_5/project_11_5/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [BASE_DIR / "staic"]
+STATICFILES_DIRS = [
+    BASE_DIR/"static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
