de2f9094fb9a2e97822b3505d50c0d63f66a9c9e
shafayetejaman <shafayetejaman@gmail.com>
2024-03-11 12:49:48 +0000

    Mon, Mar 11, 2024, 6:49 PM +06:00

--- assignment 3/restaurant/meal/urls.py
+++ assignment 3/restaurant/meal/urls.py
@@ -20,6 +20,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("", views.index),
+    path("", views.index, name="index")
 
 ]
--- assignment 3/restaurant/restaurant/urls.py
+++ assignment 3/restaurant/restaurant/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("meal/", include("meal.urls")),
+    path("about_us/", include("about_us.urls")),
 ]
