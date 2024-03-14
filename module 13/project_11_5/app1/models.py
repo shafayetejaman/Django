f488087eeaeb7647c20a92e8bb0c35728fee26d6
shafayetejaman <shafayetejaman@gmail.com>
2024-03-14 10:13:21 +0000

    Thu, Mar 14, 2024, 4:13 PM +06:00

--- module 13/project_11_5/app1/models.py
+++ module 13/project_11_5/app1/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 # Create your models here.
 class DBStudent(models.Model):
-    name = models.
+    name = models.CharField(max_length=50)
+    _id = models.IntegerField(primary)
