f98cad80d235ad474cbd56daa6481b1a927fb973
shafayetejaman <shafayetejaman@gmail.com>
2024-03-14 08:38:51 +0000

    Thu, Mar 14, 2024, 2:38 PM +06:00

--- module 13/project_11_5/app1/views.py
+++ module 13/project_11_5/app1/views.py
@@ -5,12 +5,12 @@ from .django_forms import StudentForm
 
 
 def index(request):
-    return render(request, "app1/index.html")
-
-
-def result(request):
     form = StudentForm(request.POST)
     if form.is_valid():
         print(form.changed_data)
+    return render(request, "app1/index.html", {"form": form})
 
-    return render(request, "app1/result.html", {"form": form})
+
+def result(request):
+
+    return render(request, "app1/result.html",)
