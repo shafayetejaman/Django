f96f5732f0be528f5d88f457716d36bb4ee59947
shafayetejaman <shafayetejaman@gmail.com>
2024-03-14 04:58:43 +0000

    Thu, Mar 14, 2024, 10:58 AM +06:00

--- module 13/project_11_5/app1/django_forms.py
+++ module 13/project_11_5/app1/django_forms.py
@@ -4,4 +4,4 @@ from django import forms
 class student_form(forms.Form):
     name = forms.CharField(label="Name")
     _id = forms.IntegerField(label="ID")
-    password = forms.
+    password = forms.PasswordInput()
