635d717a1fa59c93485150a31342f634398b2fdd
shafayetejaman <shafayetejaman@gmail.com>
2024-03-14 10:13:56 +0000

    Thu, Mar 14, 2024, 4:13 PM +06:00

--- module 13/project_11_5/app1/models.py
+++ module 13/project_11_5/app1/models.py
@@ -3,4 +3,5 @@ from django.db import models
 # Create your models here.
 class DBStudent(models.Model):
     name = models.CharField(max_length=50)
-    _id = models.IntegerField(primary)
+    ID = models.IntegerField(primary_key=True)
+    email = models.cha
