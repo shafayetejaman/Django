3a238a836ffb11862662be2e27779dad4fea9706
shafayetejaman <shafayetejaman@gmail.com>
2024-03-14 15:47:42 +0000

    Thu, Mar 14, 2024, 9:47 PM +06:00

--- module 13/project_11_5/app1/models.py
+++ module 13/project_11_5/app1/models.py
@@ -6,3 +6,6 @@ class DBStudent(models.Model):
     name = models.CharField(max_length=50)
     ID = models.IntegerField(primary_key=True)
     email = models.TextField()
+    
+    def __str__(self) -> str:
+        return f"ID: {self.ID} Name: {self.name}"
