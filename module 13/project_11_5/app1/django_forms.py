a11f4344477898b4acf3216246954452abd1eb21
shafayetejaman <shafayetejaman@gmail.com>
2024-03-14 09:06:38 +0000

    Thu, Mar 14, 2024, 3:06 PM +06:00

--- module 13/project_11_5/app1/django_forms.py
+++ module 13/project_11_5/app1/django_forms.py
@@ -3,5 +3,4 @@ from django import forms
 
 class StudentForm(forms.Form):
     name = forms.CharField(label="Enter your Name:")
-    _id = forms.IntegerField(label="Enter ID:")
-    password = forms.EmailField(label="Enter Email:")
+    email = forms.EmailField(label="Enter Email:")
--- module 13/project_11_5/app1/views.py
+++ module 13/project_11_5/app1/views.py
@@ -14,7 +14,7 @@ def index(request):
 def result(request):
     form = StudentForm(request.POST)
     if form.is_valid():
-        print(form.changed_data)
+        # print(form.changed_data)
     
         return render(request, "app1/result.html",{"form":form})
     return  render(request, "app1/result.html")
