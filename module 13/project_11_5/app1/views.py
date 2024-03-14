1f0808505271f0b10cf3f468a57a72ba8660510e
shafayetejaman <shafayetejaman@gmail.com>
2024-03-14 05:29:20 +0000

    Thu, Mar 14, 2024, 11:29 AM +06:00

--- module 13/project_11_5/app1/views.py
+++ module 13/project_11_5/app1/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
-from . django_forms import StudentForm
+from .django_forms import StudentForm
+
 # Create your views here.
 
 
 def index(request):
     return render(request, "app1/index.html")
 
+
 def result(request):
     form = StudentForm(request.POST)
     if form.is_valid():
         print(form.changed_data)
-        return render(request, "app1/result.html", )
+
+    return render(request, "app1/result.html", {f"form": form})
