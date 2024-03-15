a315683c67d7a446dd78b80a4d22b0fc70bab642
shafayetejaman <shafayetejaman@gmail.com>
2024-03-15 11:32:56 +0000

    Fri, Mar 15, 2024, 5:32 PM +06:00

--- module 14.5/project_14_5/project_14_5/settings.py
+++ module 14.5/project_14_5/project_14_5/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "app1"
+    "app1",
 ]
 
 MIDDLEWARE = [
@@ -117,9 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-
-STATICFILES_URL = [BASE_DIR / "static"]
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
