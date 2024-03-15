e998c181af2b2051fa38bcc8d784833ca74606f8
shafayetejaman <shafayetejaman@gmail.com>
2024-03-15 11:14:47 +0000

    Fri, Mar 15, 2024, 5:14 PM +06:00

--- module 14.5/project_14_5/app1/urls.py
+++ module 14.5/project_14_5/app1/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1.views import index
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("", index)
 ]
