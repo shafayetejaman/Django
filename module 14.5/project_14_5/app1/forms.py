d41ed848b47c7d8fe0becf38dd480922f43c40ba
shafayetejaman <shafayetejaman@gmail.com>
2024-03-17 06:14:43 +0000

    Sun, Mar 17, 2024, 12:14 PM +06:00

--- module 14.5/project_14_5/app1/forms.py
+++ module 14.5/project_14_5/app1/forms.py
@@ -4,10 +4,11 @@ from datetime import date
 
 class MyForm(forms.Form):
     name = forms.CharField(
-        label="Name"
+        label="Name",
         max_length=10,
         widget=forms.TextInput(attrs={"class": "bg-black text-white"}),
     )
     email = forms.EmailField(label="Email", required=False, initial="name@gmail.come"),
-    log = forms.BooleanField(label="Keep Login")
-    day = forms.DateField(initial=date.today)
+    
+    log = forms.BooleanField(label="Keep Login"))
+    # day = forms.DateField(initial=date.today),
