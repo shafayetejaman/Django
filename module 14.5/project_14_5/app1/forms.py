09995773199eb108cdaca1038e2172bb0875b32e
shafayetejaman <shafayetejaman@gmail.com>
2024-03-17 06:17:45 +0000

    Sun, Mar 17, 2024, 12:17 PM +06:00

--- module 14.5/project_14_5/app1/forms.py
+++ module 14.5/project_14_5/app1/forms.py
@@ -6,10 +6,8 @@ class MyForm(forms.Form):
     name = forms.CharField(
         label="Name",
         max_length=10,
-        widget=forms.TextInput(attrs={"class": "bg-black text-white"})
-    ),
-    email = forms.EmailField(label="Email", required=False, initial="name@gmail.come"),
-   
-
-    log = forms.BooleanField(label="Keep Login"),
-    day = forms.DateField(initial=date.today),
+        widget=forms.TextInput(attrs={"class": "bg-black text-white"}),
+    )
+    email = forms.EmailField(label="Email", required=False, initial="name@gmail.com")
+    log = forms.BooleanField(label="Keep Login", initial="clicked")
+    day = forms.DateField(initial=date.today)
