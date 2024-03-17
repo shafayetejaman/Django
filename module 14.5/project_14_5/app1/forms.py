2dc7517dd7cf42f9e950e0d9cd614c5e227cca83
shafayetejaman <shafayetejaman@gmail.com>
2024-03-17 05:58:30 +0000

    Sun, Mar 17, 2024, 11:58 AM +06:00

--- module 14.5/project_14_5/app1/forms.py
+++ module 14.5/project_14_5/app1/forms.py
@@ -3,5 +3,7 @@ from django import forms
 
 class MyForm(forms.Form):
     name = forms.CharField(
-        max_length=10, widget=forms.TextInput(attrs={"class": "bg-black text-white"})
+        max_length=10,
+        widget=forms.TextInput(attrs={"class": "bg-black text-white"}),
     )
+    email = forms.EmailField()
