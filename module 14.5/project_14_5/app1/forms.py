d857b9bb8ad6b495af459efd26a4509ba6a8cf60
shafayetejaman <shafayetejaman@gmail.com>
2024-03-17 05:54:57 +0000

    Sun, Mar 17, 2024, 11:54 AM +06:00

--- module 14.5/project_14_5/app1/forms.py
+++ module 14.5/project_14_5/app1/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class MyForm(forms.Form):
     name = forms.CharField(
-        max_length=10, widget=forms.CharField(attrs={"class": "bg-black text-white"})
+        max_length=10, widget=forms.TextInput(attrs={"class": "bg-black text-white"})
     )
