b6ca21ad5eb7d01263a8849fd93846db944367d2
shafayetejaman <shafayetejaman@gmail.com>
2024-03-17 04:44:27 +0000

    Sun, Mar 17, 2024, 10:44 AM +06:00

--- module 14.5/project_14_5/project_14_5/settings.py
+++ module 14.5/project_14_5/project_14_5/settings.py
@@ -38,7 +38,13 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "app1",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+# django-crispy-forms
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
