8cc646d3d8434058791964102819256516fad92e
shafayetejaman <shafayetejaman@gmail.com>
2024-03-19 11:45:44 +0000

    Tue, Mar 19, 2024, 5:45 PM +06:00

--- module 14.5/project_14_5/project_14_5/urls.py
+++ module 14.5/project_14_5/project_14_5/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls, name="Admin"),
+    path("admin/", admin.site.urls),
     path("app/", include("app1.urls"))
 ]
