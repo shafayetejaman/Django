9473c7ea318a01e230d8e82593d9417bcc24ea04
shafayetejaman <shafayetejaman@gmail.com>
2024-03-19 04:47:40 +0000

    Tue, Mar 19, 2024, 10:47 AM +06:00

--- module 14.5/project_14_5/app1/views.py
+++ module 14.5/project_14_5/app1/views.py
@@ -25,4 +25,23 @@ def index(request):
     return render(request, "app1/index.html", {"form": form})
 
 def db(request):
-    return render(request, "app1/dbpage.html")
+    if request.method == "POST":
+        form = MyForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            file = data["profile_img"]
+            with open("./app1/static/app1/images/" + file.name, "wb+") as des:
+                for i in file.chunks():
+                    des.write(i)
+
+            form = MyForm()
+            return render(
+                request,
+                "app1/dbpage.html",
+                {"form": form, "data": data},
+            )
+
+    form = MyForm()
+    return render(request, "app1/dbpage.html", {"form": form})
+   
