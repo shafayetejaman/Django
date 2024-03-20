7da521f0744d614c2625c150071db649f9fa8cda
shafayetejaman <shafayetejaman@gmail.com>
2024-03-20 10:07:50 +0000

    Wed, Mar 20, 2024, 4:07 PM +06:00

--- module 15.5/musician_directory/album/models.py
+++ module 15.5/musician_directory/album/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+Choice=(())
 
 # Create your models here.
 class albums(models.Model):
     album_name = models.CharField(max_length=20)
     release_date = models.DateField()
-    rating = models.
+    rating = models.CharField(choice=)
