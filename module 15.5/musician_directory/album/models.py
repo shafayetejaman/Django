660b43fd03a6bf1a30643f58c4b97916f1718a4f
shafayetejaman <shafayetejaman@gmail.com>
2024-03-20 10:42:42 +0000

    Wed, Mar 20, 2024, 4:42 PM +06:00

--- module 15.5/musician_directory/album/models.py
+++ module 15.5/musician_directory/album/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from .
 
 
 # Create your models here.
 class albums(models.Model):
     album_name = models.CharField(max_length=20)
     release_date = models.DateField()
-    rating = models.CharField(null=True, default=None)
+    rating = models.CharField(default=None)
+    musician = models.ForeignKey(musician, on_delete=models.CASCADE)
