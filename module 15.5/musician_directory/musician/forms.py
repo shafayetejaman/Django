6d2317f3ea176e4c3b8d6a5f4e234077a8b5deb7
shafayetejaman <shafayetejaman@gmail.com>
2024-03-23 07:05:23 +0000

    Sat, Mar 23, 2024, 1:05 PM +06:00

--- module 15.5/musician_directory/musician/forms.py
+++ module 15.5/musician_directory/musician/forms.py
@@ -1,20 +1,10 @@
 from django import forms
 from album.models import albums
 
-choice = [
-    ("web_design", "Web Design"),
-    ("seo", "Search Engine Optimization"),
-    ("content_creation", "Content Creation"),
-    ("social_media", "Social Media Management"),
-    ("branding", "Branding"),
-]
+
 
 class album_forms(forms.ModelForm):
     class Meta:
         model = albums
         fields = "__all__"
-        widgets = {
-            "instrument_type": forms.MultipleChoiceField(
-                widget=forms.CheckboxSelectMultiple, choices=choice
-            )
-        }
+       
