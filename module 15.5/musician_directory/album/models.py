13969be22219d787fdc6f43b57827a1ec685693c
shafayetejaman <shafayetejaman@gmail.com>
2024-03-23 07:04:48 +0000

    Sat, Mar 23, 2024, 1:04 PM +06:00

--- module 15.5/musician_directory/album/models.py
+++ module 15.5/musician_directory/album/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 from musician_directory.musician import musician
 
+choice = [
+    ("1", "1"),
+    ("2", "2"),
+    ("3", "3"),
+    ("4", "4"),
+    ("5", "5"),
+]
+
 
 # Create your models here.
 class albums(models.Model):
     album_name = models.CharField(max_length=20)
     release_date = models.DateField()
-    rating = models.CharField(default=None)
+    rating = models.CharField(choices=choice, default=None, max_length=50)
     musician = models.ForeignKey(musician, on_delete=models.CASCADE)
