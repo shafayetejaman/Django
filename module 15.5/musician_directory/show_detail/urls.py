9dd3b626e700b290b2f4e5e87931eb2789bdf72a
shafayetejaman <shafayetejaman@gmail.com>
2024-03-23 16:37:39 +0000

    Sat, Mar 23, 2024, 10:37 PM +06:00

--- module 15.5/musician_directory/show_detail/urls.py
+++ module 15.5/musician_directory/show_detail/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path
+from .views import index
 
-urlpatterns = []
+urlpatterns = [
+    path("", index, name="view_shows"),
+]
