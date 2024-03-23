890f09319d2d42ff667fb98c69d67d929a6b5619
shafayetejaman <shafayetejaman@gmail.com>
2024-03-23 11:14:23 +0000

    Sat, Mar 23, 2024, 5:14 PM +06:00

--- module 15.5/musician_directory/album/urls.py
+++ module 15.5/musician_directory/album/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from views import index
 
 urlpatterns = [
-    path("", )
+    path("", index)
 ]
