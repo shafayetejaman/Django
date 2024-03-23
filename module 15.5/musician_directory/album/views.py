f4fae09ab1a636aa87386b55f6bedd4e31970750
shafayetejaman <shafayetejaman@gmail.com>
2024-03-23 11:16:49 +0000

    Sat, Mar 23, 2024, 5:16 PM +06:00

--- module 15.5/musician_directory/album/views.py
+++ module 15.5/musician_directory/album/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 from forms import albums
 
 # Create your views here.
-def 
+def index(request):
+    if request.method == "POST":
+        form = albums(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            
+            form = albums()
+            return render(
+                request,
+                "album/index.html",
+                {"form": form, "data": data},
+            )
+
+    form = albums()
+    return render(request, "album/index.html", {"form": form})
