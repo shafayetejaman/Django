a3fbb882c52bdc74b4dbe0b97a3d240518e8bcea
shafayetejaman <shafayetejaman@gmail.com>
2024-03-23 16:38:49 +0000

    Sat, Mar 23, 2024, 10:38 PM +06:00

--- module 15.5/musician_directory/show_detail/views.py
+++ module 15.5/musician_directory/show_detail/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from .forms import musicians_forms
 
-# Create your views here.
+
+# # Create your views here.
+# def index(request):
+#     if request.method == "POST":
+#         form = musicians_forms(request.POST)
+#         if form.is_valid():
+#             data = form.cleaned_data
+
+#             form = musicians_forms()
+#             return render(
+#                 request,
+#                 "musician/index.html",
+#                 {"form": form, "data": data},
+#             )
+
+#     form = musicians_forms()
+#     return render(request, "musician/index.html", {"form": form})
