eefc177bd76b476af9b5142ad7cbb87610f0fdf9
shafayetejaman <shafayetejaman@gmail.com>
2024-03-23 15:01:10 +0000

    Sat, Mar 23, 2024, 9:01 PM +06:00

--- module 15.5/musician_directory/musician/models.py
+++ module 15.5/musician_directory/musician/models.py
@@ -3,16 +3,8 @@ from django.db import models
 
 # Create your models here.
 class musicians(models.Model):
-    choice = [
-        ("guitar", "Guitar"),
-        ("piano", "Piano"),
-        ("drums", "Drums"),
-        ("violin", "Violin"),
-        ("flute", "Flute"),
-    ]
-    
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
     phone_number = models.IntegerField()
-    instrument_type = models.CharField(max_length=50, choices=choice, null=True,blank=True)
+    instrument_type = models.CharField(max_length=50,required, null=True,blank=True)
