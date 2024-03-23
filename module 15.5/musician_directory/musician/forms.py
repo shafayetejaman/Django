300cb467b624d535ab9fa1b4b96e2200fe828184
shafayetejaman <shafayetejaman@gmail.com>
2024-03-23 05:22:21 +0000

    Sat, Mar 23, 2024, 11:22 AM +06:00

--- module 15.5/musician_directory/musician/forms.py
+++ module 15.5/musician_directory/musician/forms.py
@@ -1,18 +1,20 @@
 from django import forms
 from album.models import albums
 
+choice = [
+    ("web_design", "Web Design"),
+    ("seo", "Search Engine Optimization"),
+    ("content_creation", "Content Creation"),
+    ("social_media", "Social Media Management"),
+    ("branding", "Branding"),
+]
+
 class album_forms(forms.ModelForm):
-    CHOICES = [
-        ("web_design", "Web Design"),
-        ("seo", "Search Engine Optimization"),
-        ("content_creation", "Content Creation"),
-        ("social_media", "Social Media Management"),
-        ("branding", "Branding"),
-    ]
     class Mata:
         model = albums
         fields = "__all__"
-        mul   
-      = forms.MultipleChoiceField(
-        widget=forms.CheckboxSelectMultiple, choices=CHOICES
-    )
+        widgets = {
+            "instrument_type": forms.MultipleChoiceField(
+                widget=forms.CheckboxSelectMultiple, choices=choice
+            )
+        }
