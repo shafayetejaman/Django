cdc0e3b670f179c6ad6bc059bffd9a219548bf39
shafayetejaman <shafayetejaman@gmail.com>
2024-03-23 04:48:01 +0000

    Sat, Mar 23, 2024, 10:48 AM +06:00

--- module 15.5/musician_directory/album/forms.py
+++ module 15.5/musician_directory/album/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from album.models import album
+from album.models import albums
 
 class album_forms(forms.ModelForm):
     class Mata:
-        model = 
+        model = albums
+        fields = "__all__"
