fc536ff939ab9761fa4b85e4c4291b554495a914
shafayetejaman <shafayetejaman@gmail.com>
2024-03-23 04:51:02 +0000

    Sat, Mar 23, 2024, 10:51 AM +06:00

--- module 15.5/musician_directory/musician/models.py
+++ module 15.5/musician_directory/musician/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class musicians(models.Model):
+    firs_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.IntegerField()
