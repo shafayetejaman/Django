ecfaed283cccdb45fa715913c99438079e9687dd
shafayetejaman <shafayetejaman@gmail.com>
2024-03-25 09:49:47 +0000

    Mon, Mar 25, 2024, 3:49 PM +06:00

--- assignment 4/task_management_system/task/urls.py
+++ assignment 4/task_management_system/task/urls.py
@@ -19,4 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-urlpatterns = [path("", views.index, name="task")]
+urlpatterns = [
+    path("", views.index, name="task"),
+    path("edit/<int:id>", views.edit, name="edit_task"),
+    path("delete/<int:id>", views.delete, name="delete_task"),
+]
--- assignment 4/task_management_system/task/views.py
+++ assignment 4/task_management_system/task/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import TaskForms
+from .models import Task
 
 # Create your views here.
 def index(request):
@@ -12,3 +13,24 @@ def index(request):
 
     form = TaskForms()
     return render(request, "task/index.html", {"form": form})
+
+
+def edit(request, id):
+    model_data = Task.objects.get(pk=id)
+    form = TaskForms(instance=model_data)
+
+    if request.method == "POST":
+        form = TaskForms(request.POST, instance=model_data)
+        if form.is_valid():
+            form.save()
+
+            return redirect("home")
+
+    return render(request, "task/index.html", {"form": form})
+
+
+def delete(request, id):
+    model_data = Task.objects.get(pk=id)
+    model_data.delete()
+
+    return redirect("home")
