e4e7b2520c3d665fa5331c8c223aed0bbc57761a
shafayetejaman <shafayetejaman@gmail.com>
2024-04-07 11:28:22 +0000

    Sun, Apr 7, 2024, 5:28 PM +06:00

--- module 18.5 /project_18_5/app/views.py
+++ module 18.5 /project_18_5/app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .forms import Register
 
+
 # Create your views here.
 def index(request):
     form = Register()
-    return render(request, "app/index.html", {"form", form})
+
+    return render(request, "app/index.html", {"form": form})
