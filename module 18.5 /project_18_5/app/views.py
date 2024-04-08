fe0721d57e61e54ef511d46b3d3dc3c0c00c3922
shafayetejaman <shafayetejaman@gmail.com>
2024-04-08 08:07:34 +0000

    Mon, Apr 8, 2024, 2:07 PM +06:00

--- module 18.5 /project_18_5/app/views.py
+++ module 18.5 /project_18_5/app/views.py
@@ -3,11 +3,13 @@ from .forms import Register
 
 
 # Create your views here.
+
 def index(request):
+    form = Register()
     if request.method == "POST":
         form = Register(request.POST)
         if form.is_valid():
             form.save()
-
-    form = Register()
+            form = Register()
+            
     return render(request, "app/index.html", {"form": form})
