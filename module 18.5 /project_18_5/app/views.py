cd82989a249a3556ea6ff58280b106a368b9371f
shafayetejaman <shafayetejaman@gmail.com>
2024-04-12 09:58:25 +0000

    Fri, Apr 12, 2024, 3:58 PM +06:00

--- module 18.5 /project_18_5/app/views.py
+++ module 18.5 /project_18_5/app/views.py
@@ -10,6 +10,9 @@ def index(request):
     if request.method == "POST":
         form = Register(request.POST)
         messages.success(request, "Account Created Successfully!")
+        messages.warning(request, "Account Not Creation Failed!")
+        messages.info(request, "Account Not Creation Failed!")
+        
         if form.is_valid():
             form.save()
             form = Register()
