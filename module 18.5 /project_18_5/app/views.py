dde797da7973aac5f93b8ed92a63fdce3dc04b68
shafayetejaman <shafayetejaman@gmail.com>
2024-04-12 06:31:28 +0000

    Fri, Apr 12, 2024, 12:31 PM +06:00

--- module 18.5 /project_18_5/app/views.py
+++ module 18.5 /project_18_5/app/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from .forms import Register
-
+from django.contrib import messages
 
 # Create your views here.
+
 
 def index(request):
     form = Register()
     if request.method == "POST":
         form = Register(request.POST)
+        messages.success(request, "Account Created Successfully!")
         if form.is_valid():
             form.save()
             form = Register()
-            
+
     return render(request, "app/index.html", {"form": form})
