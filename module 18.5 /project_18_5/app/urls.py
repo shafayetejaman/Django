9b8af2493ab32b591df0aa35f38611d0c2fa1d9f
shafayetejaman <shafayetejaman@gmail.com>
2024-04-17 18:51:57 +0000

    Thu, Apr 18, 2024, 12:51 AM +06:00

--- module 18.5 /project_18_5/app/urls.py
+++ module 18.5 /project_18_5/app/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import user_signup, user_login, home, user_logout, pass
+from .views import user_signup, user_login, home, user_logout, password_change
 
 urlpatterns = [
     path("signup/", user_signup, name="signup"),
