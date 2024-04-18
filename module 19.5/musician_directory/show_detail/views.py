0c2344387219ef280f557e26de16fd785be33102
shafayetejaman <shafayetejaman@gmail.com>
2024-04-18 06:41:44 +0000

    Thu, Apr 18, 2024, 12:41 PM +06:00

--- module 19.5/musician_directory/show_detail/views.py
+++ module 19.5/musician_directory/show_detail/views.py
@@ -1,66 +1,9 @@
+from django.shortcuts import render
 from album.models import albums
-from django.shortcuts import render, redirect
-from .forms import Register
-from django.contrib import messages
-from django.contrib.auth import authenticate, logout, login, update_session_auth_hash
-from django.contrib.auth.forms import (
-    AuthenticationForm,
-    PasswordChangeForm,
-    SetPasswordForm,
-)
+
 
 # Create your views here.
-
 def index(request):
     data = albums.objects.all()
     print(data)
     return render(request, "show_detail/index.html", {"data": data})
-
-
-def user_signup(request):
-    form = Register()
-    if request.method == "POST":
-        form = Register(request.POST)
-
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Account Created Successfully!")
-            return redirect("login")
-        else:
-            messages.warning(request, "Account Creation Failed!")
-
-    return render(request, "app/index.html", {"form": form})
-
-
-def user_login(request):
-    if request.user.is_authenticated:
-        return redirect("home")
-
-    form = AuthenticationForm()
-    if request.method == "POST":
-        form = AuthenticationForm(request, request.POST)
-
-        if form.is_valid():
-            username = form.cleaned_data["username"]
-            password = form.cleaned_data["password"]
-            user = authenticate(username=username, password=password)
-
-            if user is not None:
-                login(request, user)
-                return redirect("home")
-        else:
-            messages.warning(request, "Login Failed!")
-
-    return render(request, "app/index.html", {"form": form})
-
-
-def home(request):
-    if request.user.is_authenticated:
-        return render(request, "app/home.html", {"user": request.user})
-
-    return redirect("signup")
-
-
-def user_logout(request):
-    logout(request)
-    return redirect("login")
