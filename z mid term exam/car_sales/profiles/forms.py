e9b7659d59d5e83a6c6a31c73bce171c03934c85
shafayetejaman <shafayetejaman@gmail.com>
2024-04-23 18:08:24 +0000

    Wed, Apr 24, 2024, 12:08 AM +06:00

--- z mid term exam/car_sales/profiles/forms.py
+++ z mid term exam/car_sales/profiles/forms.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm
-from django.urls import reverse_lazy
-from django.views.generic import UpdateView
 
 
 class UserChangeForm(UserChangeForm):
@@ -9,12 +7,3 @@ class UserChangeForm(UserChangeForm):
         model = User
         fields = ["username", "email"]
 
-
-class UserUpdateView(UpdateView):
-    model = User
-    form_class = UserChangeForm
-    template_name = "profiles/edit_profile.html"
-    success_url = reverse_lazy("home")
-
-    def get_object(self):
-        return self.request.user
