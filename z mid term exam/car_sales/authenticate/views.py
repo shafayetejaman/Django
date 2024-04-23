058fe48492d2f4d7378283ec69509bda3b14f535
shafayetejaman <shafayetejaman@gmail.com>
2024-04-23 14:47:16 +0000

    Tue, Apr 23, 2024, 8:47 PM +06:00

--- z mid term exam/car_sales/authenticate/views.py
+++ z mid term exam/car_sales/authenticate/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import Register
 from django.contrib import messages
-from django.contrib.auth import logout, update_session_auth_hash
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.contrib.auth.views import LoginView
 from django.views.generic.edit import CreateView
-
 from django.urls import reverse_lazy
 
 
@@ -57,13 +55,3 @@ def user_logout(request):
     logout(request)
     return redirect("login")
 
-
-@method_decorator(login_required, name="dispatch")
-class PasswordChangeView(PasswordChangeView):
-    template_name = "authenticate/pass_change.html"
-    success_url = reverse_lazy("login")
-
-    def form_valid(self, form):
-        messages.success(self.request, "Password Updated Successfully!")
-        update_session_auth_hash(self.request, form.user)
-        return super().form_valid(form)
