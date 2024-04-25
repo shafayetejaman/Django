c5b5fa326f591073acc4e384cd739de697a260b8
shafayetejaman <shafayetejaman@gmail.com>
2024-04-25 18:53:24 +0000

    Fri, Apr 26, 2024, 12:53 AM +06:00

--- z mid term exam/car_sales/authenticate/views.py
+++ z mid term exam/car_sales/authenticate/views.py
@@ -23,7 +23,11 @@ class UserSignupView(CreateView):
     def form_invalid(self, form):
         messages.warning(self.request, "Account Creation Failed!")
         return super().form_invalid(form)
-    
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("home")
+        return super().dispatch(request, *args, **kwargs)
 
 
 class UserLoginView(LoginView):
@@ -33,13 +37,17 @@ class UserLoginView(LoginView):
         return reverse_lazy("home")
 
     def form_valid(self, form):
-        messages.success(self.request, "Logged In Successfully")
+        messages.success(self.request, "Logged In Successful")
         return super().form_valid(form)
 
     def form_invalid(self, form):
         messages.warning(self.request, "Login Failed!")
         return super().form_invalid(form)
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("home")
+        return super().dispatch(request, *args, **kwargs)
 
 
 @login_required()
