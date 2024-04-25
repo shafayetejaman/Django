855eb001fdde000f9b30ea928fb3087869a39e51
shafayetejaman <shafayetejaman@gmail.com>
2024-04-25 16:55:57 +0000

    Thu, Apr 25, 2024, 10:55 PM +06:00

--- z mid term exam/car_sales/show_cars/views.py
+++ z mid term exam/car_sales/show_cars/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from 
 from django.views.generic import CreateView,UpdateView,DeleteView,DetailView
 
 # Create your views here.
-class CreateC
+class CreateCarPostView(CreateView):
+    model = 
