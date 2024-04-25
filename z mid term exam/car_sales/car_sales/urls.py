1cd8d63d8e06cc6b28564623efef00e8e5e52556
shafayetejaman <shafayetejaman@gmail.com>
2024-04-25 19:14:41 +0000

    Fri, Apr 26, 2024, 1:14 AM +06:00

--- z mid term exam/car_sales/car_sales/urls.py
+++ z mid term exam/car_sales/car_sales/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from car_sales.show_cars import views
+from car_sales.show_cars.views import home
 from . import settings
 from django.conf.urls.static import static
 
@@ -26,7 +26,7 @@ urlpatterns = [
     path("accounts/", include("authenticate.urls")),
     path("profile/", include("profiles.urls")),
     path("show/", include("show_cars.urls")),
-    path("", view.home, name="home"),
+    path("", home, name="home"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
