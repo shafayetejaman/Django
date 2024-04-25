bae3c5c2bb94f12daa2347ae14fb9f1ec6a07515
shafayetejaman <shafayetejaman@gmail.com>
2024-04-25 17:00:52 +0000

    Thu, Apr 25, 2024, 11:00 PM +06:00

--- z mid term exam/car_sales/show_cars/views.py
+++ z mid term exam/car_sales/show_cars/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from .models import Brand,Car,Comment
 from .forms import BrandForm,CarForm,CommentForm
 from django.views.generic import CreateView,UpdateView,DeleteView,DetailView
+from django.urls import reverse_lazy
 
 # Create your views here.
 class CreateCarPostView(CreateView):
     model = Car
     form_class = CarForm
-    template_name = "show_cars/show_list.html"
-    success_url = 
+    template_name = "show_cars/make_car.html"
+    success_url = reverse_lazy()
