99e3f17fec4a4c1f20c43fb2e259f9280d60148d
shafayetejaman <shafayetejaman@gmail.com>
2024-04-25 10:52:57 +0000

    Thu, Apr 25, 2024, 4:52 PM +06:00

--- z mid term exam/car_sales/show_cars/forms.py
+++ z mid term exam/car_sales/show_cars/forms.py
@@ -6,13 +6,19 @@ class CarForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = "__all__"
-        
+
 class BrandForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = "__all__"
         
+    def __str__(self) -> str:
+       self.name
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Car
         fields=["name", "email"]
+
+    def __str__(self) -> str:
+        self.name
