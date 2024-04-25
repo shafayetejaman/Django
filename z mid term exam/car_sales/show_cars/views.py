84a88a7a517316ecd65b68de6cc2580faa5715ea
shafayetejaman <shafayetejaman@gmail.com>
2024-04-25 17:10:00 +0000

    Thu, Apr 25, 2024, 11:10 PM +06:00

--- z mid term exam/car_sales/show_cars/views.py
+++ z mid term exam/car_sales/show_cars/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Brand,Car,Comment
-from .forms import BrandForm,CarForm,CommentForm
+from .forms import CarForm,CommentForm
 from django.views.generic import CreateView,UpdateView,DeleteView,DetailView
 from django.urls import reverse_lazy
 
@@ -8,12 +8,18 @@ from django.urls import reverse_lazy
 def show_list(request):
     return render(request, "show_cars/show_list",{"logged":request.user.is_authenticated})
 
-class CreateCarPostView(CreateView):
+class CreatePostView(CreateView):
     model = Car
     form_class = CarForm
-    template_name = "show_cars/make_car.html"
+    template_name = "show_cars/make_post.html"
     success_url = reverse_lazy("show")
 
     def is_valid(self, form):
         form.instance.author = self.request.user
         return super().is_valid(form)
+    
+class DeletePostView(DetailView):
+    model = Car
+    form_class = CarForm
+    template_name = "show_cars/delete_post.html"
+    success_url = reverse_lazy("show")
