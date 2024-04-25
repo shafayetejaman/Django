a4e09b8748b26a407bc2ee4ac9f097e584827705
shafayetejaman <shafayetejaman@gmail.com>
2024-04-25 17:03:18 +0000

    Thu, Apr 25, 2024, 11:03 PM +06:00

--- z mid term exam/car_sales/show_cars/views.py
+++ z mid term exam/car_sales/show_cars/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 
 # Create your views here.
 def show_list(request):
-    return render(request, "show_cars/make_car.html")
+    return render(request, "show_cars/show_list",{"logged":})
 
 class CreateCarPostView(CreateView):
     model = Car
