fe28c2fad7c93b5b1729a427804ba4299085f734
shafayetejaman <shafayetejaman@gmail.com>
2024-04-25 17:04:33 +0000

    Thu, Apr 25, 2024, 11:04 PM +06:00

--- z mid term exam/car_sales/show_cars/urls.py
+++ z mid term exam/car_sales/show_cars/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from .views import show_list
 
 urlpatterns = [
-    path("", name="show_list"),
+    path("",show_list, name="show"),
 ]
