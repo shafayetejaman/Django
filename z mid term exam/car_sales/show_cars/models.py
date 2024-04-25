4e013c4a1dac902b00a326812fdb80ff7ef23dc0
shafayetejaman <shafayetejaman@gmail.com>
2024-04-25 09:43:33 +0000

    Thu, Apr 25, 2024, 3:43 PM +06:00

--- z mid term exam/car_sales/show_cars/models.py
+++ z mid term exam/car_sales/show_cars/models.py
@@ -4,13 +4,13 @@ from django.db import models
 # Create your models here.
 class Brand(models.Model):
     name = models.CharField(max_length=50)
-    
-    
+
+
 class Car(models.Model):
     name = models.CharField(max_length=50)
     image = models.ImageField(upload_to="uploads/")
     description = models.TextField()
     quantity = models.IntegerField()
     price = models.FloatField()
-    slug = models.SlugField(max_length=100,unique=True)
-    bland = models.
+    slug = models.SlugField(max_length=100, unique=True)
+    bland = Pk
