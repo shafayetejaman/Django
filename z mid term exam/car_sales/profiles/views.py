fc38c3746e5e0846728ce5f277a776e4f931b02f
shafayetejaman <shafayetejaman@gmail.com>
2024-04-25 16:29:34 +0000

    Thu, Apr 25, 2024, 10:29 PM +06:00

--- z mid term exam/car_sales/profiles/views.py
+++ z mid term exam/car_sales/profiles/views.py
@@ -39,7 +39,7 @@ class UserUpdateView(UpdateView):
     form_class = UserChangeFormClass
     template_name = "profiles/edit_profile.html"
     success_url = reverse_lazy("home")
-    
+
     def post(self, request, *args, **kwargs):
-        self.object = self.get_object(request.user)
+        self.object = self.get_object()
         return super().post(request, *args, **kwargs)
