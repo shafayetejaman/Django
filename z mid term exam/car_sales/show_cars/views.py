fcf20dd4bddff2ba7ead8d283c74d379bfce5d2c
shafayetejaman <shafayetejaman@gmail.com>
2024-04-26 13:21:50 +0000

    Fri, Apr 26, 2024, 7:21 PM +06:00

--- z mid term exam/car_sales/show_cars/views.py
+++ z mid term exam/car_sales/show_cars/views.py
@@ -53,12 +53,14 @@ class DetailPostView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["logged"] = self.request.user.is_authenticated
-        post = self.object
+        car = self.object
         comments = post.comments.all()
         comment_form = CommentForm()
+        
+        context["logged"] = self.request.user.is_authenticated
         context['comments'] = comments
         context['comment_form'] = comment_form
+        
         return context
 
 
