fa620b049895de8eac7f84c2e6a1b3ad7ffa527a
shafayetejaman <shafayetejaman@gmail.com>
2024-04-26 13:47:43 +0000

    Fri, Apr 26, 2024, 7:47 PM +06:00

--- z mid term exam/car_sales/show_cars/views.py
+++ z mid term exam/car_sales/show_cars/views.py
@@ -49,29 +49,28 @@ class DetailPostView(DetailView):
     model = Car
     pk_url_kwarg = "id"
     template_name = "show_cars/detail_post.html"
-    success_url = reverse_lazy("home")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         car = self.get_object()
         comments = car.comments.all()
         comment_form = CommentForm()
-        
+
         context["logged"] = self.request.user.is_authenticated
-        context['comments'] = comments
-        context['comment_form'] = comment_form
-        
+        context["comments"] = comments
+        context["comment_form"] = comment_form
+
         return context
-    
+
     def post(self, request, *args, **kwargs):
         comment_form = CommentForm(data=self.request.POST)
         car = self.get_object()
-        
+
         if comment_form.is_valid():
             new_comment = comment_form.save(commit=False)
             new_comment.car = car
             new_comment.save()
-            
+
         return self.get(request, *args, **kwargs)
 
 
