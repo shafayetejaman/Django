64ffc28f49e13875c6cfdb26c5eaa03f5c964372
shafayetejaman <shafayetejaman@gmail.com>
2024-04-26 13:35:12 +0000

    Fri, Apr 26, 2024, 7:35 PM +06:00

--- z mid term exam/car_sales/show_cars/models.py
+++ z mid term exam/car_sales/show_cars/models.py
@@ -27,8 +27,8 @@ class Comment(models.Model):
     name = models.CharField(max_length=50)
     text = models.TextField()
     email = models.EmailField()
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name="comments")
     created = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self) -> str:
         return self.name
