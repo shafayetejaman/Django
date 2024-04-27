5b08fed43c65e3bebd69bd554c61c713ced1909b
shafayetejaman <shafayetejaman@gmail.com>
2024-04-27 14:44:41 +0000

    Sat, Apr 27, 2024, 8:44 PM +06:00

--- mid term exam/car_sales/profiles/models.py
+++ mid term exam/car_sales/profiles/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from 
 
 # Create your models here.
 
 
 class History(models.Model):
-    user = models.IntegerField()
-    car_id = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
     date_bought = models.DateTimeField(auto_now_add=True)
