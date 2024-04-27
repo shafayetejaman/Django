9c205f2eb3f3f11fb476058a3da5a6e75d99e438
shafayetejaman <shafayetejaman@gmail.com>
2024-04-27 11:29:43 +0000

    Sat, Apr 27, 2024, 5:29 PM +06:00

--- mid term exam/car_sales/profiles/models.py
+++ mid term exam/car_sales/profiles/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from show_cars.models import Car
-
+from django.contrib.auth.models import User
 # Create your models here.
+
 class history(models.Model):
+    user = User
     
