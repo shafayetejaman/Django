15a55c9ef54d7e51503a596aff1a1485883efc7b
shafayetejaman <shafayetejaman@gmail.com>
2024-04-27 14:55:48 +0000

    Sat, Apr 27, 2024, 8:55 PM +06:00

--- mid term exam/car_sales/profiles/models.py
+++ mid term exam/car_sales/profiles/models.py
@@ -8,4 +8,4 @@ from show_cars.models import Car
 class History(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    date_bought = models.DateTimeField(auto_now_add=True)
+    parched = models.DateTimeField(auto_now_add=True)
