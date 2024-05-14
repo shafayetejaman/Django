843e7ea9ce0c08202e4cbca54ef9d5ad3f78320c
shafayetejaman <shafayetejaman@gmail.com>
2024-05-14 17:26:53 +0000

    Tue, May 14, 2024, 11:26 PM +06:00

--- mamar_bank_Part1_2_Code/mamar_bank/accounts/constants.py
+++ mamar_bank_Part1_2_Code/mamar_bank/accounts/constants.py
@@ -1,8 +1,8 @@
 ACCOUNT_TYPE = (
-    ("Savings", "Savings"),
-    ("Current", "Current"),
+    ('Savings', 'Savings'),
+    ('Current', 'Current'),
 )
 GENDER_TYPE = (
-    ("Male", "Male"),
-    ("Female", "Female"),
+    ('Male', 'Male'),
+    ('Female', 'Female'),
 )
