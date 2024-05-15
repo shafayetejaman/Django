af5f3f58878a2cbedac5da7b444dcf0811f84c8d
shafayetejaman <shafayetejaman@gmail.com>
2024-05-15 11:07:38 +0000

    Wed, May 15, 2024, 5:07 PM +06:00

--- mamar_bank_Part1_2_Code/mamar_bank/transactions/urls.py
+++ mamar_bank_Part1_2_Code/mamar_bank/transactions/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("loan_request/", LoanRequestView.as_view(), name="loan_request"),
     path("loans/", LoanListView.as_view(), name="loan_list"),
     path("loans/<int:loan_id>/", PayLoanView.as_view(), name="pay"),
+    path("transfer/", .as_view(), name="")
 ]
