fe48f88dd559d139267a901692cc1d48a840cab8
shafayetejaman <shafayetejaman@gmail.com>
2024-05-15 08:21:53 +0000

    Wed, May 15, 2024, 2:21 PM +06:00

--- mamar_bank_Part1_2_Code/mamar_bank/transactions/forms.py
+++ mamar_bank_Part1_2_Code/mamar_bank/transactions/forms.py
@@ -58,7 +58,7 @@ class WithdrawForm(TransactionForm):
                 "You can not withdraw more than your account balance"
             )
 
-        if not account.transactions.withdraw_access:
+        if account.transactions.filter(withdraw_access=False):
             raise forms.ValidationError(
                 "The bank is bankrupt, you can not withdraw the money"
             )
