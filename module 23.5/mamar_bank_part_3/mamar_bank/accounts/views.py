3fb7dafe5bc06a5b4b0279c13da0fe1dfb34c85d
shafayetejaman <shafayetejaman@gmail.com>
2024-05-16 13:56:59 +0000

    Thu, May 16, 2024, 7:56 PM +06:00

--- module 23.5/mamar_bank_part_3/mamar_bank/accounts/views.py
+++ module 23.5/mamar_bank_part_3/mamar_bank/accounts/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views import View
 from django.shortcuts import redirect
+from django.views.generic import CreateView, UpdateView
 
 class UserRegistrationView(FormView):
     template_name = 'accounts/user_registration.html'
@@ -18,7 +19,7 @@ class UserRegistrationView(FormView):
         login(self.request, user)
         print(user)
         return super().form_valid(form) # form_valid function call hobe jodi sob thik thake
-    
+
 class UserLoginView(LoginView):
     template_name = 'accounts/user_login.html'
     def get_success_url(self):
@@ -31,18 +32,10 @@ class UserLogoutView(LogoutView):
         return reverse_lazy('home')
 
 
-class UserBankAccountUpdateView(View):
-    template_name = 'accounts/profile.html'
+class UserBankAccountUpdateView(UpdateView):
+    template_name = "accounts/profile.html"
+    form_class = UserUpdateForm
+    success_url = reverse_lazy("profile")
 
-    def get(self, request):
-        form = UserUpdateForm(instance=request.user)
-        return render(request, self.template_name, {'form': form})
-
-    def post(self, request):
-        form = UserUpdateForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect('profile')  # Redirect to the user's profile page
-        return render(request, self.template_name, {'form': form})
-    
-    
+    def get_object(self, queryset=None):
+        return self.request.user
