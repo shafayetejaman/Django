c9a2cfb63c340b8731eebe5d45e540ce3af772ae
shafayetejaman <shafayetejaman@gmail.com>
2024-05-16 18:22:25 +0000

    Fri, May 17, 2024, 12:22 AM +06:00

--- module 23.5/mamar_bank_part_3/mamar_bank/transactions/views.py
+++ module 23.5/mamar_bank_part_3/mamar_bank/transactions/views.py
@@ -233,4 +233,6 @@ class TransferMoneyView(TransactionCreateMixin):
             f'Successfully transfer {"{:,.2f}".format(float(amount))}$ from your account to {receiver_account.user.first_name}',
         )
 
+        send_transaction_email(self.request.user,amount,)
+
         return super().form_valid(form)
