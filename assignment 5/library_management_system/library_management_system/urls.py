882648ce0beafc8ea8efa4664eacbf55dc5b4179
shafayetejaman <shafayetejaman@gmail.com>
2024-05-20 13:34:06 +0000

    Mon, May 20, 2024, 7:34 PM +06:00

--- assignment 5/library_management_system/library_management_system/urls.py
+++ assignment 5/library_management_system/library_management_system/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for library_management_system project.
+URL configuration for car_sales project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,9 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("authenticate.urls")),
+    path("profile/", include("profiles.urls")),
+
+  
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
