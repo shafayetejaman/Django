a3787b9bd6a0e58fa563b8d4ebb9ff5c3fbec4df
shafayetejaman <shafayetejaman@gmail.com>
2024-05-22 10:32:46 +0000

    Wed, May 22, 2024, 4:32 PM +06:00

--- assignment 5/library_management_system/books/models.py
+++ assignment 5/library_management_system/books/models.py
@@ -4,11 +4,12 @@ from django.db import models
 class Book(models.Model):
     # title, description,image, borrowing price, user reviews
     title = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="uploads/")
     description = models.TextField()
-    price = models.IntegerField()
-    image = models.ImageField()
-
-
+    price = models.FloatField()
+    slug = models.SlugField(max_length=100, unique=True)
+    category = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
