aaca920a9ccbd5ed07a77c6272dbc4b4a2997e30
shafayetejaman <shafayetejaman@gmail.com>
2024-05-22 10:28:42 +0000

    Wed, May 22, 2024, 4:28 PM +06:00

--- assignment 5/library_management_system/books/models.py
+++ assignment 5/library_management_system/books/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    name = models.CharField(max_length=20)
+    # title, description,image, borrowing price, user reviews
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.IntegerField()
+    image = models.ImageField()
+
