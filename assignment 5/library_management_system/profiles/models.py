8764f7b8ca92a6397164572f76118be78986feb5
shafayetejaman <shafayetejaman@gmail.com>
2024-05-22 10:51:51 +0000

    Wed, May 22, 2024, 4:51 PM +06:00

--- assignment 5/library_management_system/profiles/models.py
+++ assignment 5/library_management_system/profiles/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from books.models import BOOK
+from books.models import Book
 
 # Create your models here.
 
-
 class History(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    car = models.ForeignKey(BOOK, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     parched = models.DateTimeField(auto_now_add=True)
