87cd442ddc0e81a27e6e84b553bd0c04a6529310
shafayetejaman <shafayetejaman@gmail.com>
2024-05-22 10:33:25 +0000

    Wed, May 22, 2024, 4:33 PM +06:00

--- assignment 5/library_management_system/books/models.py
+++ assignment 5/library_management_system/books/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+    
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
 class Book(models.Model):
     # title, description,image, borrowing price, user reviews
     title = models.CharField(max_length=50)
@@ -8,11 +16,18 @@ class Book(models.Model):
     description = models.TextField()
     price = models.FloatField()
     slug = models.SlugField(max_length=100, unique=True)
-    category = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     
-class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
+    def __str__(self) -> str:
+        return self.name
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=50)
+    text = models.TextField()
+    email = models.EmailField()
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name="comments")
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.name
