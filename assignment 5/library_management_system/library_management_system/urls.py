c23dc8dfb6b1fc413844465221fdfe0c95755aaf
shafayetejaman <shafayetejaman@gmail.com>
2024-05-22 11:08:46 +0000

    Wed, May 22, 2024, 5:08 PM +06:00

--- assignment 5/library_management_system/library_management_system/urls.py
+++ assignment 5/library_management_system/library_management_system/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from books.views import home
 from . import settings
 from django.conf.urls.static import static
 
@@ -24,8 +25,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("authenticate.urls")),
     path("profile/", include("profiles.urls")),
-    path("",include("books.urls"))
-  
+    path("", include("books.urls")),
+    path("", home, name="home"),
 ]
 
 if settings.DEBUG:
