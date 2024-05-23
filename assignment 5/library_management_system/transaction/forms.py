fabe84fd277d1d7449bb25e8c128716fc45943ed
shafayetejaman <shafayetejaman@gmail.com>
2024-05-23 15:49:48 +0000

    Thu, May 23, 2024, 9:49 PM +06:00

---
+++ assignment 5/library_management_system/transaction/forms.py
@@ -0,0 +1,74 @@
+from django import forms
+from .models import Transaction
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = [
+            'amount',
+            'transaction_type'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        self.account = kwargs.pop('account') # account value ke pop kore anlam
+        super().__init__(*args, **kwargs)
+        self.fields['transaction_type'].disabled = True # ei field disable thakbe
+        self.fields['transaction_type'].widget = forms.HiddenInput() # user er theke hide kora thakbe
+
+    def save(self, commit=True):
+        self.instance.account = self.account
+        self.instance.balance_after_transaction = self.account.balance
+        return super().save()
+
+
+class DepositForm(TransactionForm):
+    def clean_amount(self): # amount field ke filter korbo
+        min_deposit_amount = 100
+        amount = self.cleaned_data.get('amount') # user er fill up kora form theke amra amount field er value ke niye aslam, 50
+        if amount < min_deposit_amount:
+            raise forms.ValidationError(
+                f'You need to deposit at least {min_deposit_amount} $'
+            )
+
+        return amount
+
+
+
+
+class LoanRequestForm(TransactionForm):
+    def clean_amount(self):
+        amount = self.cleaned_data.get('amount')
+
+        return amount
+
+
+class TransferForm(TransactionForm):
+    def clean_amount(self):
+        account = self.account
+        min_transfer_amount = 500
+        max_transfer_amount = 20000
+        balance = account.balance  # 1000
+        amount = self.cleaned_data.get("amount")
+
+        if amount < min_transfer_amount:
+            raise forms.ValidationError(
+                f"You can transfer at least {min_transfer_amount} $"
+            )
+
+        if amount > max_transfer_amount:
+            raise forms.ValidationError(
+                f"You can transfer at most {max_transfer_amount} $"
+            )
+
+        if amount > balance:  # amount = 5000, tar balance ache 200
+            raise forms.ValidationError(
+                f"You have {balance} $ in your account. "
+                "You can not transfer more than your account balance"
+            )
+
+        if not account.withdraw_access:
+            raise forms.ValidationError(
+                "The bank is bankrupt, you can not transfer the money"
+            )
+
+        return amount
