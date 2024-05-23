f50047942b6054118de92a11f74dc4f2bd2d755c
shafayetejaman <shafayetejaman@gmail.com>
2024-05-23 13:46:56 +0000

    Thu, May 23, 2024, 7:46 PM +06:00

--- assignment 5/library_management_system/account/forms.py
+++ assignment 5/library_management_system/account/forms.py
@@ -1,6 +1,8 @@
 from .models import UserAccount
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from datetime import datetime
+
 
 class UserRegistrationForm(UserCreationForm):
     class Meta:
@@ -11,14 +13,15 @@ class UserRegistrationForm(UserCreationForm):
             "last_name",
             "email",
         ]
+
     def save(self, commit=True):
         user = super().save(commit=False)
-        
+
+        current_date = datetime.now()
+        date = int(current_date.strftime("%Y%m%d"))
+
         if commit:
             user.save()
-            UserAccount.objects.create(
-                user=user,
-                account_no=1000+user.id
-            )
-        
+            UserAccount.objects.create(user=user, account_no=date + user.id)
+
         return user
