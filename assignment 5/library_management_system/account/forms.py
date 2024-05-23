50f1bd017c6880a1a106f48dde2191bb47763a3a
shafayetejaman <shafayetejaman@gmail.com>
2024-05-23 12:36:01 +0000

    Thu, May 23, 2024, 6:36 PM +06:00

--- assignment 5/library_management_system/account/forms.py
+++ assignment 5/library_management_system/account/forms.py
@@ -1,6 +1,6 @@
 from .models import UserAccount
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.models import User
 
 class UserRegistrationForm(UserCreationForm):
     class Meta:
