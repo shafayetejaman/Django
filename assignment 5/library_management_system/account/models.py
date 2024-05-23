be580c8f1d45de4767582c66268e97299af5432c
shafayetejaman <shafayetejaman@gmail.com>
2024-05-23 12:29:15 +0000

    Thu, May 23, 2024, 6:29 PM +06:00

--- assignment 5/library_management_system/account/models.py
+++ assignment 5/library_management_system/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserAccount(models.Model):
+    user = models.OneToOneField(User, related_name="account", on_delete=models.CASCADE)
+    account_no = models.IntegerField(
+        unique=True
+    )  # account no duijon user er kokhono same hobe na
+    birth_date = models.DateField(null=True, blank=True)
