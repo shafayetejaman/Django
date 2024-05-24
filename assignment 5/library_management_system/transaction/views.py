fd708dea47a9593998493339adaeb28d35067959
shafayetejaman <shafayetejaman@gmail.com>
2024-05-24 15:08:55 +0000

    Fri, May 24, 2024, 9:08 PM +06:00

--- assignment 5/library_management_system/transaction/views.py
+++ assignment 5/library_management_system/transaction/views.py
@@ -5,10 +5,35 @@ from books.models import Book
 # Create your views here.
 
 
-def buy_book(request, id):
-    book = Book.objects.get(pk=id)
-    book.save()
+class DepositMoneyView(TransactionCreateMixin):
+    form_class = DepositForm
+    title = "Deposit"
 
-    History.objects.create(book=book, user=request.user)
+    def get_initial(self):
+        initial = {"transaction_type": DEPOSIT}
+        return initial
 
-    return redirect("home")
+    def form_valid(self, form):
+        amount = form.cleaned_data.get("amount")
+        account = self.request.user.account
+        # if not account.initial_deposit_date:
+        #     now = timezone.now()
+        #     account.initial_deposit_date = now
+        account.balance += (
+            amount  # amount = 200, tar ager balance = 0 taka new balance = 0+200 = 200
+        )
+        account.save(update_fields=["balance"])
+
+        messages.success(
+            self.request,
+            f'{"{:,.2f}".format(float(amount))}$ was deposited to your account successfully',
+        )
+        send_transaction_email(
+            self.request.user,
+            amount,
+            "Deposite Message",
+            "transactions/deposite_email.html",
+        )
+        return super().form_valid(form)
+
+
