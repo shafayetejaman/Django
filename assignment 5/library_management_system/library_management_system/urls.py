c5e9f3b6359c8fca7caa96cafe8c6c8b1a592b7f
shafayetejaman <shafayetejaman@gmail.com>
2024-05-26 13:18:38 +0000

    Sun, May 26, 2024, 7:18 PM +06:00

--- assignment 5/library_management_system/library_management_system/urls.py
+++ assignment 5/library_management_system/library_management_system/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for car_sales project.
+URL configuration for book_sales project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
